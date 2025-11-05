df0da24519f6ae4cb074add8debad3faee20f2f7
Robin wayne <146124302+ROBIN241223@users.noreply.github.com>
2025-11-05 14:32:14 +0700

    tạo freertos_cli.h

---
+++ Core/Inc/freertos_cli.h
@@ -0,0 +1,28 @@
+#ifndef COMMAND_INTERPRETER_H
+#define COMMAND_INTERPRETER_H 
+
+#define configCOMMAND_INT_MAX_OUTPUT_SIZE 16384
+
+typedef BaseType_t (*pdCOMMAND_LINE_CALLBACK)( char * pcWriteBuffer, size_t xWriteBufferLen, const char * pcCommandString );
+
+typedef struct xCOMMAND_LINE_INPUT
+{
+    const char * const pcCommand;
+    const char * const pcHelpString;
+    const pdCOMMAND_LINE_CALLBACK pxCommandInterpreter;
+} CLI_Command_Definition_t;
+
+#define xCommandLineInput CLI_Command_Definition_t
+
+BaseType_t FreeRTOS_CLIRegisterCommand( const CLI_Command_Definition_t * const pxCommandToRegister );
+
+BaseType_t FreeRTOS_CLIProcessCommand( const char * const pcCommandInput, char * pcWriteBuffer, size_t xWriteBufferLen );
+
+char * FreeRTOS_CLIGetOutputBuffer( void );
+
+const char * FreeRTOS_CLIGetParameter( const char * pcCommandString, UBaseType_t uxWantedParameter, BaseType_t * pxParameterStringLength );
+
+extern void vUARTCommandConsoleStart( uint16_t usStackSize, UBaseType_t uxPriority );
+extern void vRegisterSampleCLICommands( void );
+
+#endif
